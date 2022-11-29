ae85f151c9fd067a055bf22e5db1b84efd1edb7f
I-Maged <magedhanafy@gmail.com>
2022-11-29 10:58:40 +0200

    year & text fix

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Manage-landing-page
+
+This project is a [frontendmentor.io](https://www.frontendmentor.io/challenges/manage-landing-page-SLXqC6P5) challenge to create a responsive landing page.
+
+## Tech used
+
+- HTML
+- Tailwind CSS
+- JavaScript
+
+![Preview](/img/screen.png?raw=true)
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Year
+const thisYear = document.querySelectorAll('.year');
+const currentYear = new Date().getFullYear();
+for (let i = 0; i < thisYear.length; i++) {
+  thisYear[i].innerHTML = currentYear;
+}
+
+// Hambuger Menu
 const btn = document.getElementById('menu-btn');
 const nav = document.getElementById('menu');
 
